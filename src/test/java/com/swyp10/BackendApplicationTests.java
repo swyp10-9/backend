24e0499af03ede1b8f0413e15afd39649be5c655
Ahnyeongjun <toinin@naver.com>
2025-07-25 13:40:15 +0900

    fix: test용 security key 증설 & test profile 사용

--- src/test/java/com/swyp10/BackendApplicationTests.java
+++ src/test/java/com/swyp10/BackendApplicationTests.java
@@ -1,9 +1,16 @@
 package com.swyp10;
 
+import com.swyp10.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"
+})
+@ActiveProfiles("test")
+@Import(TestConfig.class)
 class BackendApplicationTests {
 
     @Test
