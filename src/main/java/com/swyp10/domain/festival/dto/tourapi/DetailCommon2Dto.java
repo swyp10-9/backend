40fcbe993b013070724b385f43d32acf6e8a8c00
Ahnyeongjun <toinin@naver.com>
2025-08-11 18:21:46 +0900

    fix: 모든 dto 정의

--- src/main/java/com/swyp10/domain/festival/dto/tourapi/DetailCommon2Dto.java
+++ src/main/java/com/swyp10/domain/festival/dto/tourapi/DetailCommon2Dto.java
@@ -7,4 +7,31 @@ import lombok.*;
 @Builder
 public class DetailCommon2Dto {
     private String overview;
+    private String contentid;
+    private String sigungucode;
+    private String cat1;
+    private String cat2;
+    private String cat3;
+    private String addr1;
+    private String addr2;
+    private String zipcode;
+    private String mapx;
+    private String mapy;
+    private String mlevel;
+    private String cpyrhtDivCd;
+    private String contenttypeid;
+    private String createdtime;
+    private String homepage;
+    private String modifiedtime;
+    private String tel;
+    private String telname;
+    private String title;
+    private String firstimage;
+    private String firstimage2;
+    private String areacode;
+    private String lDongRegnCd;
+    private String lDongSignguCd;
+    private String lclsSystm1;
+    private String lclsSystm2;
+    private String lclsSystm3;
 }
--- src/main/java/com/swyp10/domain/festival/dto/tourapi/SearchFestival2Dto.java
+++ src/main/java/com/swyp10/domain/festival/dto/tourapi/SearchFestival2Dto.java
@@ -1,29 +1,39 @@
 package com.swyp10.domain.festival.dto.tourapi;
 
 import lombok.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
 @Builder
 public class SearchFestival2Dto {
-    private String contentid;
+    private String lclsSystm3;
+    private String tel;
+    private String title;
     private String addr1;
+    private String addr2;
     private String areacode;
+    private String cat1;
+    private String cat2;
+    private String cat3;
+    private String contentid;
     private String contenttypeid;
     private String createdtime;
     private String eventstartdate;
+    private String cpyrhtDivCd;
     private String eventenddate;
     private String firstimage;
     private String firstimage2;
     private String mapx;
     private String mapy;
+    private String mlevel;
     private String modifiedtime;
     private String sigungucode;
-    private String tel;
-    private String title;
+    private String zipcode;
+    private String progresstype;
+    private String festivaltype;
     private String lDongRegnCd;
     private String lDongSignguCd;
     private String lclsSystm1;
-    private String progresstype;
-    private String festivaltype;
+    private String lclsSystm2;
 }
